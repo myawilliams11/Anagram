47285e09616bf97c299874753da2881a7cad5408
Mya <35534673+myawilliams11@users.noreply.github.com>
2018-07-18 18:23:05 -0400

    Add files via upload

--- file.js
+++ file.js
@@ -3,13 +3,13 @@ document.getElementById("findButton").onclick = function () {
     console.log("typedText is " + typedText);
     var alphabetizedTypedText = alphabetize(typedText);     // "dgo"
     console.log("alphabetizedTypedText is " + alphabetizedTypedText)
-
+    document.getElementById("result")
     for (i = 0; i < words.length; i++) {
         var currentWord = words[i];                             // "god"
         var alphabetizedCurrentWord = alphabetize(currentWord); // "dgo"
 
         if (alphabetizedTypedText == alphabetizedCurrentWord) {
-            document.write(currentWord + " ");
+            result.textContent+=currentWord + " ";
         }
 
     }
@@ -21,15 +21,3 @@ document.getElementById("findButton").onclick = function () {
 function alphabetize(a) {
     return a.toLowerCase().split("").sort().join("").trim();
 }
-// function alphabetize(words) {
-//     return b.toLowerCase().split("").sort().join("").trim();
-// }
-
-// Find Anagram
-
-//    not sure if i need this
-// alphabetize("lair") gives "ailr"
-// alphabetize("liar") gives "ailr"
-// alphabetize("rail") gives "ailr"
-// alphabetize("least") gives "aelst"
-// alphabetize("steal") gives "aelst"
